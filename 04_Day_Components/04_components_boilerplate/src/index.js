3bcc0dadaea4b2486d9bcc435adbd2c1e60f3b9e
Kevin Hui <kevinhui98@gmail.com>
2022-09-15 21:01:15 -0400

    complete day4

--- 04_Day_Components/04_components_boilerplate/src/index.js
+++ 04_Day_Components/04_components_boilerplate/src/index.js
@@ -1,7 +1,8 @@
 // index.js
 import React from 'react'
 import ReactDOM from 'react-dom'
-import asabenehImage from './images/asabeneh.jpg'
+import kevinImage from './images/kevin.png'
+import './index.css'
 
 const hexaColor = () => {
   let str = '0123456789abcdef'
@@ -25,6 +26,7 @@ const HexaColor = () => {
     borderRadius: '5px',
     width: '75%',
     border: '2px solid black',
+    'background-color': bgColor,
   }
   return (
     <div style={styles}>
@@ -33,55 +35,56 @@ const HexaColor = () => {
   )
 }
 
-// Header Component
-const Header = () => (
-  <header>
-    <div className='header-wrapper'>
-      <h1>Welcome to 30 Days Of React</h1>
-      <h2>Getting Started React</h2>
-      <h3>JavaScript Library</h3>
-      <p>Asabeneh Yetayeh</p>
-      <small>Oct 3, 2020</small>
-    </div>
-  </header>
-)
 
 // User Card Component
 const UserCard = () => (
   <div className='user-card'>
-    <img src={asabenehImage} alt='asabeneh image' />
-    <h2>Asabeneh Yetayeh</h2>
+    <img src={kevinImage} alt='kevin' />
+    <h2>kevin h</h2>
   </div>
 )
 
 // TechList Component
 const TechList = () => {
-  const techs = ['HTML', 'CSS', 'JavaScript']
-  const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
+  const techs = ['html', 'css', 'Javascript', 'Git', 'ReactJS', 'Python', 'C++', 'Java', 'MySQL', 'Kotlin']
+  const techsFormatted = techs.map((tech) => <button style={buttonStyles} key={tech} id={tech}>{tech}</button>)
   return techsFormatted
 }
 const buttonStyles = {
   padding: '10px 20px',
-  background: 'rgb(0, 255, 0)',
+  background: hexaColor(),
   border: 'none',
   borderRadius: 5,
+  margin: '0 5px',
 }
-
-const Button = () => <button style={buttonStyles}> action </button>
-
+const input = {
+  'border-radius': '10px',
+  'margin-left': '10px',
+  'margin-right': '10px',
+  'height': '30px',
+}
 // Main Component
 const Main = () => (
   <main>
     <div className='main-wrapper'>
-      <p>Prerequisite to get started react.js:</p>
+      <UserCard />
       <ul>
         <TechList />
       </ul>
-      <UserCard />
       <div>
         {/* Generate two different hexa colors every time */}
         <HexaColor />
         <HexaColor />
+        {/* <Button /> */}
+      </div>
+      <div id='SubscribeField'>
+        <h1>SUBSCRIBE</h1>
+        <p>Sign up with your email address to receive news and updates</p>
+        <input style={input} id="FirstName" placeholder=' FirstName' />
+        <input style={input} id="LastName" placeholder=' LastName' />
+        <input style={input} id="Email" placeholder=" Email" />
+        <br></br>
+        <button placeholder='Subscrible' id='Subscrible'>Subscrible</button>
       </div>
     </div>
   </main>
@@ -99,7 +102,6 @@ const Footer = () => (
 // The App, or the parent or the container component
 const App = () => (
   <div className='app'>
-    <Header />
     <Main />
     <Footer />
   </div>
